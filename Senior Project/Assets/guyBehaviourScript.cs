d5fc693f3bb709b058692d390ae6a90e87548bdd
kevinkunker <72618644+kevinkunker@users.noreply.github.com>
2021-10-22 14:33:42 -0400

    Add an animation for 48x48 character

--- Senior Project/Assets/guyBehaviourScript.cs
+++ Senior Project/Assets/guyBehaviourScript.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class guyBehaviourScript : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator m_Animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        m_Animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,15 +22,21 @@ public class guyBehaviourScript : MonoBehaviour
             if (Input.GetKey(KeyCode.A))
             {
                 rb.velocity = new Vector2(-5, rb.velocity.y);
+                m_Animator.SetTrigger("Walk");
             }
             if (Input.GetKey(KeyCode.D))
             {
                 rb.velocity = new Vector2(5, rb.velocity.y);
+                m_Animator.SetTrigger("Walk");
             }
             if (Input.GetKey(KeyCode.Space))
             {
                 rb.velocity = new Vector2(rb.velocity.x, 5);
             }
+        }
+        if (rb.velocity.x == 0)
+        {
+            m_Animator.SetTrigger("StopWalk");
         }
     }
 }
