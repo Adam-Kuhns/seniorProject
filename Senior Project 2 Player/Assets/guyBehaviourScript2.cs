315285160985551a2f1347a24b6ed30a26087bd8
Adam Kuhns <abk03310@vsc.edu>
2021-11-05 14:01:38 -0400

    2 player animations

---
+++ Senior Project 2 Player/Assets/guyBehaviourScript2.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guyBehaviourScript2 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator m_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKey(KeyCode.F))
+      {
+          m_Animator.SetTrigger("Attack");
+      }
+        if (rb.velocity.y == 0)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rb.velocity = new Vector2(-5, rb.velocity.y);
+                m_Animator.SetTrigger("Walk2");
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rb.velocity = new Vector2(5, rb.velocity.y);
+                m_Animator.SetTrigger("Walk2");
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 5);
+            }
+        }
+        if (rb.velocity.x == 0)
+        {
+            m_Animator.SetTrigger("StopWalk2");
+        }
+    }
+}
