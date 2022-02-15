18e97fd040731832e1280e7e618d190a626cdef6
RooplesPooples <72618678+RooplesPooples@users.noreply.github.com>
2022-02-15 10:41:12 -0500

    Blood

--- Senior Project/Assets/scripts/BulletBehavior.cs
+++ Senior Project/Assets/scripts/BulletBehavior.cs
@@ -2,13 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BulletBehavior : MonoBehaviour
 {
+    private Animator b_Animator;
+    private Rigidbody2D rb;
+    private GameObject collidedWith;
+
+    void Start()
+    {
+        b_Animator = gameObject.GetComponent<Animator>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "enemy")
-            GameObject.Destroy(collision.gameObject);
-
+        {
+            rb.velocity = new Vector2(0, 0);
+            b_Animator.SetTrigger("hit");
+            collidedWith = collision.gameObject;
+        }
+    }
+    void KillEnemy(){
         GameObject.Destroy(gameObject);
+        GameObject.Destroy(collidedWith);
     }
 }
