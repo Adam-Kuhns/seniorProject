5a213d98c4ba39392fa56680be5ef8733901d7fe
Adam Kuhns <abk03310@vsc.edu>
2022-03-01 15:32:58 -0500

    Level Select Menu

--- Senior Project/Assets/scripts/MainMenuStart.cs
+++ Senior Project/Assets/scripts/MainMenuStart.cs
@@ -8,17 +8,17 @@ public class MainMenuStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void LoadGame()
     {
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("Level Select");
     }
 }
